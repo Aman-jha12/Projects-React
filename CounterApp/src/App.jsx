d6c10a2921386794ca490b6af203496da0c5763d
Aman-jha12 <ajha82703@gmail.com>
2025-05-22 18:21:52 +0530

    Initialized an empty repo

---
+++ CounterApp/src/App.jsx
@@ -0,0 +1,14 @@
+import {useState} from "react";
+
+
+
+export default function App(){
+  const[count,setCount]=useState(0);
+  return(
+<>
+<div>
+  <Count count={count}></Count>
+</div>
+</>
+  )
+}
