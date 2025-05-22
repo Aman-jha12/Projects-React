7f275eb082a2750d4a3aaf91db498af883fa20a0
Aman-jha12 <ajha82703@gmail.com>
2025-05-22 18:34:40 +0530

    Completed

--- CounterApp/src/App.jsx
+++ CounterApp/src/App.jsx
@@ -8,7 +8,24 @@ export default function App(){
 <>
 <div>
   <Count count={count}></Count>
+  <Button count={count} setCount={setCount}></Button>
 </div>
 </>
+  )
+}
+
+function Count({count}){
+  return (
+    <div>
+      <h1>{count}</h1>
+    </div>
+  )
+}
+function Button({count,setCount}){
+  return(
+    <div>
+    <button onClick={() => setCount(count+1)}>increment</button>
+    <button onClick={() => setCount(count-1)}>Decrement</button>
+    </div>
   )
 }
